83ea06f33570c63332a036c59b24d2fa2675ea05
Le Manh Tuong <lemanhtuong2109@gmail.com>
2025-02-20 17:18:13 +0700

    push code

---
+++ ShopApp_Test/src/app/app.module.server.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+import { provideServerRouting } from '@angular/ssr';
+import { AppModule } from './app.module';
+import { serverRoutes } from './app.routes.server';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+
+@NgModule({
+  imports: [AppModule, ServerModule],
+  providers: [provideServerRouting(serverRoutes)],
+  bootstrap: [DetailProductComponent],
+})
+export class AppServerModule {}
---
+++ ShopApp_Test/src/app/order-confirm/order-confirm.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-confirm',
+  standalone: false,
+  templateUrl: './order-confirm.component.html',
+  styleUrl: './order-confirm.component.css'
+})
+export class OrderConfirmComponent {
+
+}
