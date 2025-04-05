c3f10ba0b8b17b17734c4850a831a59a3b7aa057
Le Manh Tuong <lemanhtuong2109@gmail.com>
2025-04-05 18:17:57 +0700

    setup Spring Security, create object UserDetails

--- ShopApp_Backend/Shopapp/src/main/java/com/project/Shopapp/Models/Account.java
+++ ShopApp_Backend/Shopapp/src/main/java/com/project/Shopapp/Models/Account.java
@@ -2,8 +2,14 @@ package com.project.Shopapp.Models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Setter
@@ -12,7 +18,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Table(name = "ACCOUNTS")
 @Builder
-public class Account extends BaseEntity {
+public class Account extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int USERID;
@@ -33,4 +39,46 @@ public class Account extends BaseEntity {
     private int GOOGLE_ACCOUNT_ID;
 
     private boolean ROLENAME;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String name = "User";
+        if(ROLENAME == true)
+            name = "Admin";
+
+
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("ROLE_"+ name));
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return SODIENTHOAI;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
